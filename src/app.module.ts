d1189935718a5bcb01174172de3563df52d45770
abdelali djebara <adjebara@sqli.com>
2024-07-26 19:42:13 +0100

    feat: create migrations for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'ecommerce-lab',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      migrationsTableName: 'migrations',
+      migrations: ['../src/migration/*.ts']
+    }),
+    ProductsModule,
+    CategoriesModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
