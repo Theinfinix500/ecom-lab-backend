dccbc13cf142c2867bfec63ce6b61fca2e2c39a5
Djebara Abdelali <jbaraali18@gmail.com>
2024-07-27 16:40:05 +0100

    feat: products filters - pagination - sort

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,14 +4,17 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
   UsePipes,
   ValidationPipe
 } from '@nestjs/common';
 
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -25,8 +28,25 @@ export class ProductsController {
   }
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  async findAll(
+    @Query('page', ParseIntPipe) page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('sort') sort: string = 'createdAt',
+    @Query('order') order: 'ASC' | 'DESC' = 'DESC',
+    @Query() query: { [key: string]: any }
+  ): Promise<{
+    data: Product[];
+    count: number;
+    totalPages: number;
+    currentPage: number;
+  }> {
+    const filter = { ...query };
+    delete filter.page;
+    delete filter.limit;
+    delete filter.sort;
+    delete filter.order;
+
+    return this.productsService.findAll({ page, limit, sort, order, filter });
   }
 
   @Get(':id')
